2032e3ded260329241457abacc1e5e26d2d54308
Abdoulaye Sow <psow.el45@gmail.com>
2026-07-17 17:33:41 +0200

    correct ioctl parameters on keyboardinterrupt

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 # pysniff v1
 
 import socket
+import time
+
+logo = r'''
+                       _ ______
+   ___  __ _____ ___  (_) _/ _/
+  / _ \/ // (_-</ _ \/ / _/ _/ 
+ / .__/\_, /___/_//_/_/_//_/   
+/_/   /___/                    
+
+https://github.com/0x18F
+
+'''
 
 def sniff():
-   
+        print(logo)
+        time.sleep(2)
         with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
             s.connect(('8.8.8.8', 80))
             HOST_IP = s.getsockname()[0]
@@ -15,11 +28,11 @@ def sniff():
             try:
                 while True:
                     packet, addr = s.recvfrom(65565)
-                    print(f'Packet from {addr[0]} | Size: {len(packet)} bytes')
+                    print(f'[+] Packet from {addr[0]} | Size: {len(packet)} bytes')
         
             except KeyboardInterrupt:
-                print('[-] Program ended')
-                s.ioctl(socket.RCVALL_OFF)
+                s.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+                print('[-] Program ended by user')
     
 if __name__ == "__main__":
     sniff()
